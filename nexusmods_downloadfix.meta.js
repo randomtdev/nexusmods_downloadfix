c2472d12332004b2c7e561a980d531d263e36018
randomtdev <whatameme@nexusmods.com>
2020-01-24 12:08:31 -0500

    1.4 update
    - Vortex banner is no longer being removed
    - Improved shitty patching system a bit
    - Fix for new site update
    - Cleaned up code quite a bit
    
    This new update makes getting the link for manual downloads a lot faster, courtesy of nexus staff.

--- nexusmods_downloadfix.meta.js
+++ nexusmods_downloadfix.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            Nexus Download Fix
-// @description     Fixing the absolute dogshit new download system for nexusmods
+// @description     Fixing the dumb new download system for nexusmods
 // @namespace       randomtdev
 // @include         *://www.nexusmods.com/*/mods/*?tab=files&file_id=*
 // @include         *://www.nexusmods.com/*/mods/*
 // @grant           none
-// @version         1.3.1
+// @version         1.4
 // @author          randomtdev
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @updateURL       https://gitcdn.xyz/repo/randomtdev/nexusmods_downloadfix/master/nexusmods_downloadfix.meta.js
--- nexusmods_downloadfix.user.js
+++ nexusmods_downloadfix.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            Nexus Download Fix
-// @description     Fixing the absolute dogshit new download system for nexusmods
+// @description     Fixing the dumb new download system for nexusmods
 // @namespace       randomtdev
 // @include         *://www.nexusmods.com/*/mods/*?tab=files&file_id=*
 // @include         *://www.nexusmods.com/*/mods/*
 // @grant           none
-// @version         1.3.1
+// @version         1.4
 // @author          randomtdev
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @updateURL       https://gitcdn.xyz/repo/randomtdev/nexusmods_downloadfix/master/nexusmods_downloadfix.meta.js
@@ -14,18 +14,23 @@
 // @run-at			document-start
 // ==/UserScript==
 
-// This is a script to partially work around the fact that nexus has become some shitty third-rate download site with constant (shameless) "SLOW DOWNLOAD OR PREMIUM DOWNLOAD BUY ME PLEASE I'M BETTER" prompts That also of course make you wait 5 seconds and bug you to buy premium.
-// Hopefully someone with better knowledge of the JS environment can make a better version of this; I'm not very experienced with JS or web development at all.
+// This is a script to partially work around the fact that nexus has become some shitty third-rate download site with constant (shameless) "SLOW DOWNLOAD OR PREMIUM DOWNLOAD BUY ME PLEASE I'M BETTER" prompts that also of course make you wait 5 seconds and bug you to buy premium.
 
-// Of course, this does not fix the rate limiting nor is it meant to, but it will skip the dumb 5 second wait that the admins seem to be jerking off to and basically completely restore the original experience.
-// I had no issue with the rate limiting but this shit crosses the line for me. Instantly lost all of my respect for this site's administration as soon as I saw the god awful prompt you see on every garbage download site.
-// Next thing you'll know we'll have a set amount of files we can download every couple of hours and speeds will be capped at 100KB and the user experience will get even worse
+/* Update 1.4 (2020-01-24)
 
+   Hey me; It's you, 1 month later! It's 2020 now. A new update a couple days ago by the nexus staff broke the script, under the pretense that the script didn't tell nexus it downloaded the file.
+   This is partially true, as the first versions of the script did not do this. This was fixed shortly after though. The latest version of the script did in fact do this.
+   I'm unsure if there's concern over said outdated version of the script or if it's just bullshit, but in any case it's basically a given in this version (1.4)
+   The change they made actually makes it significantly easier and faster for the script, essentially completely restoring the original experience of the site(for manual downloads). 
+   The post in question; https://old.reddit.com/r/skyrimmods/comments/ernl7e/is_anybody_else_unable_to_manually_download_files/ff563z6/
+   In case it gets deleted; https://i.imgur.com/Vovlf73.png
 
-// Anyway, you guys make some shameless cash grab and I'll shamelessly bypass it and release it. Fuck you. Once this gets fixed I'll just bypass it again
+   Thanks to the guys on github issue reporting for the reddit link & error report
+*/
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 var patchedButtons = false
+//var currentUrlParams = new URLSearchParams(new URL(document.URL))
 
 function GetDownloadButtons() {
     // nice one m8, real genius right here
@@ -42,6 +47,11 @@ function GetButtonLabel(button) {
 }
 
 function SetButtonLabel(button, text) {
+    if (!button) 
+    {
+        return
+    }
+
     var element = button.getElementsByTagName("span")[0]
     if (!element) {
         element = button
@@ -50,138 +60,132 @@ function SetButtonLabel(button, text) {
     element.innerHTML = text
 }
 
-// Tell the server we've downloaded the mod so the user can endorse it and it counts towards the DL count (whoops)
-function ActuallyDownloadTheMod(href) {
-    var q2 = $("#section.modpage")
-    if (q2.length > 0) // We only want to click endorse button if we haven't already endorsed the mod.
+function DownloadFile(href, button)
+{
+    console.log("Attempting to download file from link", href)
+
+    var q = $("#section.modpage")
+    if (q.length > 0)
     {
-        var section = q2[0]
-
+        var section = q[0]
+        var params = new URLSearchParams(new URL(href, document.URL).search)
         var gameId = section.getAttribute("data-game-id")
-        var modId = section.getAttribute("data-mod-id")
-        var fileId = href.substring(href.indexOf("&file_id=") + 9, href.length) // This should work consistently. Probably.
+        var fileId = params.get("file_id") || params.get("id")
 
-        console.log("Telling server we downloaded the mod", gameId, modId, fileId)
+        if (params.get("nmm"))
+        {
+            console.log("Requesting premium page to get NXM protocol link")
+            var originalText = GetButtonLabel(button)
 
-        // Was totally going to put an auto-endorse here but nexus only allows an endorse 15 minutes after download. welp
-        $.ajax({
-            type: "POST",
-            url: "/Core/Libs/Common/Managers/Downloads?ConfirmFinishedDownload",
-            data: {
-                fid: fileId,
-                game_id: gameId,
-            },
-            success: function () {
-                $('a svg.icon-endorse').parent().removeClass('btn-inactive');
-                $('a svg.icon-vote').parent().removeClass('btn-inactive');
-            }
-        });
+            SetButtonLabel(button, "Getting link...")
+            $.ajax({
+                url: href,
+                data: "",
+                success: function (response) {
+                    var match = response.match(/data-download-url="(.*)"/)
+                    if (!match) {
+                        alert("Download Failed! Are you logged in?")
+                        SetButtonLabel(button, originalText)
+                        return
+                    }
+                    console.log("Got nxm link ", match[1])
+                    window.location.href = match[1]
+    
+                    $('button.mfp-close').click() // Close mod requirements popup if any
 
+                    SetButtonLabel(button, "Got link!")
+
+                    setTimeout(function () {
+                        SetButtonLabel(button, originalText)
+                    }, 5000)
+                }
+            });
+        }
+        else
+        {
+            console.log("Requesting download URL for file", fileId, "with game ID", gameId)
+
+            // Guess I wont complain if we have this, and I certainly won't point out any issues with them doing this. Nope. Not at all. No issues here.
+            $.ajax(
+                {
+                    type: "POST",
+                    url: "/Core/Libs/Common/Managers/Downloads?GenerateDownloadUrl",
+                    data: {
+                        fid: fileId,
+                        game_id: gameId,
+                    },
+                    success: function (data) {
+                        if (data && data.url) {
+                            window.location.href = data.url;
+                            $('button.mfp-close').click() // Close mod requirements popup if any
+
+                            // This is pretty pointless because you can't use them, but whatever.
+                            $('a svg.icon-endorse').parent().removeClass('btn-inactive');
+                            $('a svg.icon-vote').parent().removeClass('btn-inactive');
+                        } else {
+                            alert("Download Failed! Request went through, but couldn't get URL.\n\nAre you logged in?")
+                            console.error("GenerateDownloadUrl failed; Got data ", data)
+                        }
+                    },
+                    error: function () {
+                        console.error("Download request failed for file", fileId)
+                        alert("Download request failed!")
+                    }
+                }
+            );
+        }
+
+    }
+    else
+    {
+        console.error("uhhh is the page loaded yet?")
     }
 }
 
 function PatchButton(button) {
     var link = button.getAttribute("href")
-    //button.removeAttribute("href")
-    button.addEventListener("click", function (e) {
-        e.preventDefault()
-        if (button.doingCache) {
-            button.clickedIt = true
-            return
-        }
 
-        console.log("Requesting download page", link)
-        var originalText = GetButtonLabel(button)
-        SetButtonLabel(button, "Downloading...")
-
-        if (button.cachedLink) {
-            window.location.href = button.cachedLink
-            ActuallyDownloadTheMod(link)
-            $('button.mfp-close').click()
-            return
-        }
-
-        $.ajax({
-            url: link,
-            data: "",
-            success: function (response) {
-                var match = response.match(/data-download-url="(.*)"/)
-                if (!match) {
-                    alert("Download Failed! Are you logged in?")
-                    SetButtonLabel(button, originalText)
-                    return
-                }
-                console.log("Downloading ", match[1])
-                window.location.href = match[1]
-
-                if (button.requirements) {
-                    $('button.mfp-close').click() // Close mod requirements popup
-                } else {
-                    SetButtonLabel(button, "Downloaded File!")
-                    setTimeout(function () {
-                        SetButtonLabel(button, originalText)
-                    }, 5000)
-                }
-                ActuallyDownloadTheMod(link)
-            }
-        });
-    })
-
-    if (button.requirements && !button.doingCache && !button.cachedLink) // Chances are if you've clicked the download button you want to download this. Let's grab and cache the actual download link after it's been opened instead of once the 2nd download button is clicked.
+    if (link.includes("ModRequirementsPopUp"))
     {
-        var txt = GetButtonLabel(button)
-        SetButtonLabel(button, "Preparing download...")
-        button.doingCache = true
-        console.log("Preparing download link for mod requirements popup. Requesting ", link)
-        $.ajax({
-            url: link,
-            data: "",
-            success: function (response) {
-                button.doingCache = false
-
-                var match = response.match(/data-download-url="(.*)"/)
-                if (!match) {
-                    alert("Download Failed! Are you logged in?")
-                    SetButtonLabel(button, txt)
-                    return
-                }
-
-                console.log("Cached real download link ", match[1])
-                button.cachedLink = match[1]
-
-                SetButtonLabel(button, txt)
-                if (button.clickedIt) // If the user clicked the button before it was finished preparing, we'll just initiate the download instead of having them click again.
-                {
-                    console.log("User already clicked download button; Downloading ", button.cachedLink)
-                    window.location.href = button.cachedLink
-                    $('button.mfp-close').click()
-                    ActuallyDownloadTheMod(link)
-                }
-            }
-        });
+        button.requirements = true
     }
+    
+    button.addEventListener("click", function (e) {
+        if (button.popup && button.downloaded)
+        {
+            return
+        }
+
+        if (button.requirements)
+        {
+            var findDownload = setInterval(function () {
+                var btn = $("a.btn:contains('Download')")
+                if (btn.length > 0) {
+                    console.log("patching popup download btn")
+                    clearInterval(findDownload)
+                    btn[0].popup = true
+                    PatchButton(btn[0])
+                }
+            }, 300)
+        }
+        else
+        {
+            e.preventDefault()
+
+            DownloadFile(link, button)
+            button.downloaded = true
+        }
+
+    })
 
     button.patched = true
+    //console.log("patched", button)
 }
 
-function PatchFixModRequirements(button) {
-    if (!button.getAttribute("href").includes("ModRequirementsPopUp")) {
-        return false
-    }
-
-    button.addEventListener("click", function (e) {
-        var findDownload = setInterval(function () {
-            var btn = $("a.btn:contains('Download')")
-            if (btn.length > 0) {
-                clearInterval(findDownload)
-                btn[0].requirements = true
-                PatchButton(btn[0])
-            }
-        }, 500)
-
-    })
-
-    return true
+function IsDownloadButton(button)
+{
+    var params = new URLSearchParams(new URL(button.getAttribute("href"), document.URL).search)
+    return params.get("file_id") || (params.get("id") && params.get("game_id"))
 }
 
 function PatchButtonBySpanText(text) {
@@ -191,9 +195,8 @@ function PatchButtonBySpanText(text) {
     }
     
     var btn = q[0].parentNode
-
-    // We don't want to patch this button if it just leads to the files page, because I guess that's a thing for multiple files.
-    if (!PatchFixModRequirements(btn) && btn.getAttribute("href").indexOf("&file_id=") != -1) 
+    // We don't want to patch this button if it just leads to the files page, because I guess that's a thing for mods with multiple main files.
+    if (IsDownloadButton(btn)) 
     {
         PatchButton(btn)
     }
@@ -202,7 +205,7 @@ function PatchButtonBySpanText(text) {
 }
 
 function PatchDownloadButtons() {
-    if (document.URL.indexOf("?tab=files") == -1) // This is probably fine...
+    if (document.URL.indexOf("?tab=files") == -1)
     {
         return false
     }
@@ -223,12 +226,11 @@ function PatchDownloadButtons() {
             }
             //console.log(button)
 
-            // dumb way to get the download button that's created for mod requirements
-            if (PatchFixModRequirements(button)) {
-                continue
+            if (IsDownloadButton(button))
+            {
+                PatchButton(button)
             }
-
-            PatchButton(button)
+            
         }
     }
 
@@ -240,6 +242,16 @@ function RemoveAdblockBanner() {
     if ($(".premium-banner").remove().length > 0) // oh no not the premium banner
     {
         console.log("Removed premium nag banner")
+    }
+}
+
+function InitializePatches()
+{
+    if (PatchDownloadButtons()) {
+        console.log("We did dem button patches boi")
+        patchedButtons = true
+    } else {
+        console.error("Looks like we failed to do button patches without an exception some reason. No buttons?")
     }
 }
 
@@ -264,12 +276,7 @@ function Initialize() {
 
                     console.log("postload fallback")
                     console.log("Loaded new file page; Trying to patch download buttons")
-                    if (PatchDownloadButtons()) {
-                        console.log("We did dem button patches boi")
-                        patchedButtons = true
-                    } else {
-                        console.error("Looks like we failed to do button patches without an exception some reason. No buttons?")
-                    }
+                    InitializePatches()
                 }
             }, 100)
         } else {
@@ -280,59 +287,35 @@ function Initialize() {
 
                 if ($('#mod_files').length > 0) { // Is this the files page?
                     console.log("Loaded new file page; Trying to patch download buttons")
-                    if (PatchDownloadButtons()) {
-                        console.log("We did dem button patches boi")
-                        patchedButtons = true
-                    } else {
-                        console.error("Looks like we failed to do button patches without an exception some reason. No buttons?")
-                    }
+                    InitializePatches()
                 }
                 RemoveAdblockBanner()
             }
 
             if (document.URL.indexOf("?tab=files") > -1) // Do initial patches if we're loading the files page directly.
             {
-                if (PatchDownloadButtons()) {
-                    console.log("We did dem button patches boi")
-                    patchedButtons = true
-                } else {
-                    console.error("Looks like we failed to do button patches without an exception some reason. No buttons?")
-                }
+                InitializePatches()
                 RemoveAdblockBanner()
             }
         }
 
-
-        $("#rj-vortex").remove() // Nooooo not the 124px tall vortex banner!!! (like can you get any more obnoxious please)
-        $(".agroup.clearfix").remove() // whoops there goes the ad banner
-        console.log("Removed Vortex/Ad banners")
-
-
         return
+    }
+    else
+    {
+        // This portion was here before I did the button patching; Leaving it here as a failsafe in case patching fails or bugs out.
+        // Cleaned up and adapted for new workaround.
+        var slowBtn = $('#slowDownloadButton')
+        var fastBtn = $('#fastDownloadButton')
+
+        // free shitty memes
+        slowBtn.children("span").text("BOO YOU SUCK");
+        fastBtn.children("span").text("BUY PREMIUM BRO");
+
+        console.log("Downloading file")
+
+        DownloadFile(document.URL) // No clue if it actually works properly on this page; Don't feel like testing it right now
     }
 }
 window.onload = Initialize
-// This portion was here before I did the button patching; Leaving it here as a failsafe in case patching fails or bugs out.
 
-var slowBtn = $('#slowDownloadButton')
-var fastBtn = $('#fastDownloadButton')
-
-// free shitty memes
-slowBtn.children("span").text("BOO YOU SUCK");
-fastBtn.children("span").text("BUY PREMIUM BRO");
-
-var url = slowBtn.attr("data-download-url") // Get URL directly from attribute(lmao) instead of waiting 5 seconds(also lmao what has this website become)
-if (!url) {
-    return
-}
-window.location.href = url // Redirect to actual download link
-
-// Log this download to console
-
-var file = url.substring(0, url.indexOf("?md5="))
-if (!file) {
-    return
-}
-console.log("Downloading file ", url)
-
-ActuallyDownloadTheMod(document.URL) // No clue if it actually works properly on this page; Don't feel like testing it right now
