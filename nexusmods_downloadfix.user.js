ce57f5be1461a032088f2f28603c5f3e8b2bff02
derickso <dylangerickson@gmail.com>
2024-06-16 15:52:12 -0700

    Update nexusmods_downloadfix.user.js for Premium

--- nexusmods_downloadfix.user.js
+++ nexusmods_downloadfix.user.js
@@ -5,7 +5,7 @@
 // @include         *://www.nexusmods.com/*/mods/*?tab=files&file_id=*
 // @include         *://www.nexusmods.com/*/mods/*
 // @grant           none
-// @version         1.5.2
+// @version         1.6.0
 // @author          randomtdev
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @updateURL       https://gitcdn.xyz/repo/randomtdev/nexusmods_downloadfix/master/nexusmods_downloadfix.meta.js
@@ -85,7 +85,7 @@ function DownloadFile(href, button)
                 url: href,
                 data: "",
                 success: function (response) {
-                    var match = response.match(/data-download-url="(.*)"/)
+                    var match = response.match(/id="dl_link" value="(.*)"/)
                     if (!match) {
                         DisplayPopup("DownloadFix script error", "Download Failed! Are you logged in?")
                         SetButtonLabel(button, originalText)
@@ -93,6 +93,8 @@ function DownloadFile(href, button)
                     }
                     console.log("Got nxm link ", match[1])
                     window.location.href = match[1]
+
+                    $.magnificPopup.close();
     
                     SetButtonLabel(button, "Got link!")
 
@@ -157,7 +159,7 @@ function DownloadFile(href, button)
 function PatchButton(button) {
     var link = button.getAttribute("href")
 
-    if (link.includes("ModRequirementsPopUp"))
+    if (link.includes("ModRequirementsPopUp") || link.includes("download-top-left-panel"))
     {
         return
     }
