d5717ffbc0170d23bc42570327bfce5e7cfdde71
randomtdev <58486554+randomtdev@users.noreply.github.com>
2025-02-26 14:15:52 -0600

    Update nexusmods_downloadfix.meta.js

--- nexusmods_downloadfix.meta.js
+++ nexusmods_downloadfix.meta.js
@@ -5,11 +5,9 @@
 // @include         *://www.nexusmods.com/*/mods/*?tab=files&file_id=*
 // @include         *://www.nexusmods.com/*/mods/*
 // @grant           none
-// @version         1.5.2
+// @version         1.6.0
 // @author          randomtdev
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
-// @updateURL       https://gitcdn.xyz/repo/randomtdev/nexusmods_downloadfix/master/nexusmods_downloadfix.meta.js
-// @downloadURL     https://gitcdn.xyz/repo/randomtdev/nexusmods_downloadfix/master/nexusmods_downloadfix.user.js
 // @homepageURL     https://github.com/randomtdev/nexusmods_downloadfix
 // @run-at			document-start
 // ==/UserScript==
